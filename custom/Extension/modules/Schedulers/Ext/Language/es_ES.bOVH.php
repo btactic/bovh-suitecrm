3c837bce74be75d2602c93fe7680a6c036914bd9
Marc Sánchez <sanchezfauste@gmail.com>
2016-10-20 12:20:33 +0200

    Adding custom scheduler

---
+++ custom/Extension/modules/Schedulers/Ext/Language/es_ES.bOVH.php
@@ -0,0 +1,4 @@
+<?php
+    $mod_strings['LBL_BOVH'] = 'Sincronización bOVH';
+
+
---
+++ custom/Extension/modules/Schedulers/Ext/ScheduledTasks/bOVH.php
@@ -0,0 +1,8 @@
+<?php
+
+$job_strings[] = 'bOVH';
+
+function bOVH() {
+    require_once('bOVH/main.php');
+    return main();
+}
