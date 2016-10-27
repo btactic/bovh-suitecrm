d2c4385c9ee8ec97a8de27f78c1f0003c032e55c
Marc Sánchez <sanchezfauste@gmail.com>
2016-10-27 13:46:00 +0200

    Sync MACs

--- bOVH/main.php
+++ bOVH/main.php
@@ -4,6 +4,7 @@ require_once('bOVH/ovh.php');
 require_once('bOVH/domain.php');
 require_once('bOVH/dedicated_server.php');
 require_once('bOVH/ip.php');
+require_once('bOVH/virtual_address.php');
 
 function main() {
     $GLOBALS['log']->fatal("[bOVH] Entering bOVH synchronization.");
@@ -13,6 +14,8 @@ function main() {
     $server->sync_all_dedicated_servers();
     $ip = new IP(OVH::get_api_instance());
     $ip->sync_all_ips();
+    $virtualAddress = new VirtualAddress(OVH::get_api_instance());
+    $virtualAddress->sync_all_virtual_addresses();
     return true;
 }
 
---
+++ bOVH/virtual_address.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once('bOVH/bean_utils.php');
+
+class VirtualAddress {
+
+    public function __construct($ovh_api_instance) {
+        $this->ovh = $ovh_api_instance;
+    }
+
+    public function sync_all_virtual_addresses() {
+        try {
+            $servers = $this->ovh->get('/dedicated/server');
+            foreach ($servers as $server) {
+                $this->sync_dedicated_server_virtual_addresses($server);
+            }
+        } catch (Exception $e) {
+            $GLOBALS['log']->fatal("[bOVH] Error retrieving dedicated servers list.");
+        }
+    }
+
+    public function sync_dedicated_server_virtual_addresses($servername) {
+        try {
+            $virtualMacs = $this->ovh->get('/dedicated/server/'.$servername.'/virtualMac');
+            foreach ($virtualMacs as $virtualMac) {
+                $this->sync_virtualmac_addresses($servername, $virtualMac);
+            }
+        } catch (Exception $e) {
+            $GLOBALS['log']->fatal("[bOVH] Error retrieving virtual macs list.");
+        }
+    }
+
+    private function sync_virtualmac_addresses($servername, $virtualMac) {
+        try {
+            $virtualAddresses = $this->ovh->get('/dedicated/server/'.$servername
+                    .'/virtualMac/'.$virtualMac.'/virtualAddress');
+            foreach ($virtualAddresses as $virtualAddress) {
+                $keys_values = array();
+                $keys_values['name'] = $virtualAddress;
+                $bean = retrieve_record_bean('btc_IP', $keys_values);
+                $bean->name = $virtualAddress;
+                $bean->mac = $virtualMac;
+                $bean->save();
+            }
+        } catch (Exception $e) {
+            $GLOBALS['log']->fatal("[bOVH] Error retrieving virtual addresses list.");
+        }
+    }
+
+}
+
+?>
