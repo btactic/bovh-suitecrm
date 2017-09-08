8ebe40c58cb97d8e92396b638c668d2a67f3f1a1
Marc Sánchez <sanchezfauste@gmail.com>
2017-09-08 15:19:49 +0200

    Setting only the domain owner if vat is set

--- bOVH/domain.php
+++ bOVH/domain.php
@@ -64,22 +64,24 @@ class Domain {
     }
 
     private function set_domain_owner(&$domain_bean, $owner_info) {
-        $keys_values = array();
-        $keys_values['nif_c'] = $owner_info['vat'];
-        $account_bean = retrieve_record_bean('Accounts', $keys_values);
-        if (empty($account_bean->id)) {
-            $account_bean->name = $owner_info['organisationName'];
-            $account_bean->nif_c = $owner_info['vat'];
-            $account_bean->email1 = $owner_info['email'];
-            $account_bean->phone_work = $owner_info['phone'];
-            $account_bean->billing_address_street = $owner_info['address']['line1'];
-            $account_bean->billing_address_city = $owner_info['address']['city'];
-            $account_bean->billing_address_state = $owner_info['address']['province'];
-            $account_bean->billing_address_postalcode = $owner_info['address']['zip'];
-            $account_bean->billing_address_country = $owner_info['address']['country'];
-            $account_bean->save();
+        if (!empty($owner_info['vat'])) {
+            $keys_values = array();
+            $keys_values['nif_c'] = $owner_info['vat'];
+            $account_bean = retrieve_record_bean('Accounts', $keys_values);
+            if (empty($account_bean->id)) {
+                $account_bean->name = $owner_info['organisationName'];
+                $account_bean->nif_c = $owner_info['vat'];
+                $account_bean->email1 = $owner_info['email'];
+                $account_bean->phone_work = $owner_info['phone'];
+                $account_bean->billing_address_street = $owner_info['address']['line1'];
+                $account_bean->billing_address_city = $owner_info['address']['city'];
+                $account_bean->billing_address_state = $owner_info['address']['province'];
+                $account_bean->billing_address_postalcode = $owner_info['address']['zip'];
+                $account_bean->billing_address_country = $owner_info['address']['country'];
+                $account_bean->save();
+            }
+            $domain_bean->account_id_c = $account_bean->id;
         }
-        $domain_bean->account_id_c = $account_bean->id;
     }
 
 }
