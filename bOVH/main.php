c25fc1f1f5d4315dc61ad5f9209d3d7e8f3c4551
Marc Sánchez <sanchezfauste@gmail.com>
2018-03-20 13:31:02 +0100

    Print more info to log

--- bOVH/main.php
+++ bOVH/main.php
@@ -8,12 +8,16 @@ require_once('bOVH/virtual_address.php');
 
 function main() {
     $GLOBALS['log']->fatal("[bOVH] Entering bOVH synchronization.");
+    $GLOBALS['log']->fatal("[bOVH] --> Syncing domain zones...");
     $domain = new Domain(OVH::get_api_instance());
     $domain->sync_all_domain_zones();
+    $GLOBALS['log']->fatal("[bOVH] --> Syncing dedicated servers...");
     $server = new DedicatedServer(OVH::get_api_instance());
     $server->sync_all_dedicated_servers();
+    $GLOBALS['log']->fatal("[bOVH] --> Syncing IPs...");
     $ip = new IP(OVH::get_api_instance());
     $ip->sync_all_ips();
+    $GLOBALS['log']->fatal("[bOVH] --> Syncing virtual address...");
     $virtualAddress = new VirtualAddress(OVH::get_api_instance());
     $virtualAddress->sync_all_virtual_addresses();
     $GLOBALS['log']->fatal("[bOVH] bOVH synchronization finished.");
